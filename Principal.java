8d5fa52ea191faef472756aeb2447a8cda5fdd41
joaoluizcfonseca <joaoluizcfonseca@gmail.com>
2026-03-25 20:12:03 -0300

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class exercicio4 {
+
+    public static void main(String[] args) {
+        String nome ;
+
+        nome = "Joao";
+        JOptionPane.showMessageDialog(null, "Olá " + nome + "!");
+
+    }
+}
